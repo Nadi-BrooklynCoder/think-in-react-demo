8550c8b40e97791e6c8e3fb98206bc53b609138d
nmatos <nmatos@pursuit.org>
2024-04-11 15:17:23 -0400

    inital commit

---
+++ src/Components/header/Header.jsx
@@ -0,0 +1,11 @@
+import './header.css'
+
+export default function Header () {
+
+    return (
+        <div className='header-div'>
+        <h1 className='header'>Agenda</h1>
+        <p className='subheader'>Take control of your day</p>
+        </div>
+    )
+}
---
+++ src/Components/options/Options.jsx
@@ -0,0 +1,21 @@
+import './options.css'
+
+export default function Options () {
+
+    return (
+    <div className='optionsContainer'>
+        <div className="option">
+            <p>Your Options</p>
+            <p>Remove All</p>
+        </div>
+        <div className="clean">
+            <p>1.Clean</p>
+            <p>Remove</p>
+        </div>
+        <div className="work">
+            <p>2.Work</p>
+            <p>Remove</p>
+        </div>
+        </div>
+    )
+}
